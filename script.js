f524236098a32ea9c4be076e75a60bde0ae0bd5b
nafisahusaini <116873280+nafisahusaini@users.noreply.github.com>
2024-01-19 16:53:33 +0430

    updates to the converter logic

--- script.js
+++ script.js
@@ -16,44 +16,44 @@
 
 //  console.log(timp,num)
 function Convert() {
-const timp=document.getElementById('first').value;
-const num=parseFloat(document.getElementById('num').value)
-// var celsius=0
-var fahrenheit=0
-// var kelvin=0
-// var result=document.getElementById('')
-const TO_timp=document.getElementById('second').value;
-    if(timp=="Celsius" && TO_timp=="Fahrenheit"){
-       fahrenheit= (num*9/5) + 32;
-    //    console.log(fahrenheit);
-       document.getElementById('id').innerHTML=fahrenheit;
-    }
-    else  if(timp=="Fahrenheit" && TO_timp=="Celsius"){
-        fahrenheit= (num - 32) *5/9;
-     //    console.log(fahrenheit);
-        document.getElementById('id').innerHTML=fahrenheit;
-     } 
-     else  if(timp=="Celsius" && TO_timp=="Kelvin"){
-        fahrenheit= num + 273.15;
-     //    console.log(fahrenheit);
-        document.getElementById('id').innerHTML=fahrenheit;
-     }
-     else  if(timp=="Kelvin" && TO_timp=="Celsius"){
-        fahrenheit= num - 273.15;
-     //    console.log(fahrenheit);
-        document.getElementById('id').innerHTML=fahrenheit;
-     }
-     else  if(timp=="Fahrenheit" && TO_timp=="Kelvin"){
-        fahrenheit= ((num -32)*5)/9 + 273.15;
-     //    console.log(fahrenheit);
-        document.getElementById('id').innerHTML=fahrenheit;
-     }
-     else  if(timp=="Kelvin" && TO_timp=="Fahrenheit"){
-        fahrenheit= ((num - 273.15)*9)/5 + 32;
-      
-     //    console.log(fahrenheit);
-        document.getElementById('id').innerHTML= fahrenheit;
-     }
-}
-// document.getElementById('show').addEventListener('click', Convert());
-//Convert();
+   const timp=document.getElementById('first').value;
+   const num=parseFloat(document.getElementById('num').value)
+   // var celsius=0
+   var fahrenheit=0
+   // var kelvin=0
+   // var result=document.getElementById('')
+   const TO_timp=document.getElementById('second').value;
+       if(timp=="Celsius" && TO_timp=="Fahrenheit"){
+          fahrenheit= (num*9/5) + 32;
+       //    console.log(fahrenheit);
+          document.getElementById('id').innerHTML=Math.ceil(fahrenheit);
+       }
+       else  if(timp=="Fahrenheit" && TO_timp=="Celsius"){
+           fahrenheit= (num - 32) *5/9;
+        //    console.log(fahrenheit);
+           document.getElementById('id').innerHTML=Math.ceil(fahrenheit);
+        } 
+        else  if(timp=="Celsius" && TO_timp=="Kelvin"){
+           fahrenheit= num + 273.15;
+        //    console.log(fahrenheit);
+           document.getElementById('id').innerHTML=Math.ceil(fahrenheit);
+        }
+        else  if(timp=="Kelvin" && TO_timp=="Celsius"){
+           fahrenheit= num - 273.15;
+        //    console.log(fahrenheit);
+           document.getElementById('id').innerHTML=Math.ceil(fahrenheit);
+        }
+        else  if(timp=="Fahrenheit" && TO_timp=="Kelvin"){
+           fahrenheit= ((num -32)*5)/9 + 273.15;
+        //    console.log(fahrenheit);
+           document.getElementById('id').innerHTML=Math.ceil(fahrenheit);
+        }
+        else  if(timp=="Kelvin" && TO_timp=="Fahrenheit"){
+           fahrenheit= ((num - 273.15)*9)/5 + 32;
+         
+        //    console.log(fahrenheit);
+           document.getElementById('id').innerHTML=Math.ceil(fahrenheit);
+        }
+   }
+   // document.getElementById('show').addEventListener('click', Convert());
+   //Conver
